806a0c370b6767fe750a070c83f26774014e070d
marc-antoine <marcantoine.bruelhart@gmail.com>
2020-09-17 09:11:17 +0200

    Use camunda springboot starter test

--- src/test/java/ch/mab/camunda/DevelopingProcessTest.java
+++ src/test/java/ch/mab/camunda/DevelopingProcessTest.java
@@ -1,6 +1,6 @@
 package ch.mab.camunda;
 
-import static org.junit.Assert.assertEquals;
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.assertThat;
 
 import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.TaskService;
@@ -9,12 +9,9 @@ import org.camunda.bpm.engine.task.Task;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.spring.boot.starter.test.helper.AbstractProcessEngineRuleTest;
 import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
 
 // https://docs.camunda.org/manual/7.5/user-guide/testing/
 // https://camunda.com/best-practices/testing-process-definitions/
-
-//@RunWith(SpringRunner.class)
 public class DevelopingProcessTest extends AbstractProcessEngineRuleTest {
 
     private final String KEY_DEVELOPMENT_PROCESS = "development-process";
@@ -23,34 +20,31 @@ public class DevelopingProcessTest extends AbstractProcessEngineRuleTest {
     @Deployment(resources = KEY_DEVELOPMENT_PROCESS + ".bpmn")
     public void test_demo() {
         RuntimeService runtimeService = processEngine.getRuntimeService();
-        runtimeService.startProcessInstanceByKey(KEY_DEVELOPMENT_PROCESS);
+        final ProcessInstance processInstance = runtimeService.startProcessInstanceByKey(KEY_DEVELOPMENT_PROCESS);
 
         TaskService taskService = processEngine.getTaskService();
         Task task = taskService.createTaskQuery().singleResult();
 
-        assertEquals("Commitment", task.getName());
+        assertThat(processInstance).isWaitingAt("Commitment");
     }
 
     @Test
     @Deployment(resources = KEY_DEVELOPMENT_PROCESS + ".bpmn")
     public void testDevelopmentProcess() {
         RuntimeService runtimeService = processEngine.getRuntimeService();
-        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey(KEY_DEVELOPMENT_PROCESS);
+        final ProcessInstance processInstance = runtimeService.startProcessInstanceByKey(KEY_DEVELOPMENT_PROCESS);
+
+        assertThat(processInstance).isWaitingAt("Commitment");
 
         TaskService taskService = processEngine.getTaskService();
         Task task = taskService.createTaskQuery().singleResult();
 
-        assertTaskName("Commitment", task);
+        assertThat(processInstance).isWaitingAt("Commitment");
 
         runtimeService.setVariable(task.getExecutionId(), "committed", "false");
         taskService.complete(task.getId());
 
         task = taskService.createTaskQuery().singleResult();
-        assertTaskName("Commitment", task);
+        assertThat(processInstance).isWaitingAt("Commitment");
     }
-
-    private void assertTaskName(String name, Task task) {
-        Assertions.assertEquals(name, task.getName());
-    }
-
 }
